2f97fe15dcdda43b10963822320d1dd506250b2a
vassilyo123 <31938424+vassilyo123@users.noreply.github.com>
2021-03-08 11:10:05 -0600

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'qr';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['pre'] = '';
+$config['db']['port'] = '';
+
+$config['version'] = '5.5';
+$config['installed'] = '1';
+?>
